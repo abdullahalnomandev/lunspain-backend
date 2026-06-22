6c6fa3adfa67e421548748572a0fb51795d63cd4
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2026-06-22 12:53:34 +0600

    updte

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ app.post('/api/webhook', express.raw({ type: 'application/json' }),handleStripeW
 
 //body parser
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb'}));
 
 //file retrieve
 app.use(express.static('uploads'));
